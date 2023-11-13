bfc136d46ea48234088c6abd451facb51b106aac
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-13 18:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const extendHex = (shortHex) => {
   // write your code here
+
+  let hexCode = ["#"],
+    isCapital = false;
+
+  shortHex = shortHex.replace("#", "");
+
+  for (let i = 0; i < shortHex.length; i++) {
+    if (
+      shortHex.charAt(i) === shortHex.charAt(i).toUpperCase() &&
+      shortHex.charAt(i) !== shortHex.charAt(i).toLowerCase()
+    ) {
+      isCapital = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < shortHex.length; i++) {
+    if (isCapital) {
+      hexCode.push(shortHex.charAt(i).toUpperCase());
+      hexCode.push(shortHex.charAt(i).toUpperCase());
+    } else {
+      hexCode.push(shortHex.charAt(i));
+      hexCode.push(shortHex.charAt(i));
+    }
+  }
+
+  return hexCode.join("");
 };
 
 // Do not change the code below.
